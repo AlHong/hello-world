a8332f300fedaf479f71003cf5bc1025ccecf2b7
AlHong <allanhong327@hotmail.com>
2017-02-26 14:42:27 -0500

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,8 +1,9 @@
-public class hello_world {
-    public static void main(String[] args) {
+public class hello_world 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("Hello World!");
     }
-
 }
 
 
